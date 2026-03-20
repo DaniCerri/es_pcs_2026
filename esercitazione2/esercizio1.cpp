b648197726f67310510b5c0cca84bf491c3f9143
Daniele Cerrina <daniecerri@gmail.com>
2026-03-20 12:07:05 +0000

    Fine esercizio 1

--- esercitazione2/esercizio1.cpp
+++ esercitazione2/esercizio1.cpp
@@ -9,20 +9,31 @@ int main()
     int x = 1;
     float y = 1.1;
 
-    // Stampa di alcuni indirizzi
+    // Stampa degli indirizzi
     std::cout << "Indirizzo di x: " << &x << "\n";
     std::cout << "Indirizzo di y: " << &y << "\n";
-    std::cout << "Indirizzo di ad[3]: " << &ad[3] << "\n";
-    std::cout << "Indirizzo di af[7]: " << &af[7] << "\n";
-    std::cout << "Indirizzo di ai[1]: " << &ai[1] << "\n";
+    
+    std::cout << "Indirizzi di ad: \n";
+    for (int i = 0; i < 4; i++) {
+        std::cout << "  * Indirizzo di ad[" << i << "]: " << &ad[i] << "\n";
+    }
+    std::cout << "\n";
 
+    std::cout << "Indirizzi di af: \n";
+    for (int i = 0; i < 8; i++) {
+        std::cout << "  * Indirizzo di af[" << i << "]: " << &af[i] << "\n";
+    }
+    std::cout << "\n";
+
+    std::cout << "Indirizzi di ai: \n";
+    for (int i = 0; i < 3; i++) {
+        std::cout << "  * Indirizzo di ai[" << i << "]: " << &ai[i] << "\n";
+    }
+    std::cout << "\n";
 
     
     (&y)[1] = 0; // questa riga causa un buffer overflow, infatti y è un float e non ha un secondo elemento, ma viene trattato come un array di float
     // Nota: il valore di x cambia in 0 a causa del buffer overflow ma solamente con clang, non con g++
-
-
-
 
     std::cout << x << "\n";
 
